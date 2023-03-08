8bcc50b21ba935523a1e655589c7973aba5bc8f0
Anisa Zelkia <zelkiaanisa@gmail.com>
2023-03-08 18:00:00 +0700

    create constant and update login service

--- community/src/main/java/com/lawencon/community/dao/UserDao.java
+++ community/src/main/java/com/lawencon/community/dao/UserDao.java
@@ -1,11 +1,11 @@
 package com.lawencon.community.dao;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.stereotype.Repository;
 
-import com.lawencon.base.BaseEntity;
 import com.lawencon.base.ConnHandler;
 import com.lawencon.community.model.User;
 
@@ -15,7 +15,7 @@ public class UserDao extends BaseMasterDao<User>{
 
 	@SuppressWarnings("unchecked")
 	@Override
-	List<User> getAll() {
+	public List<User> getAll() {
 		final String sql = "SELECT * FROM t_user WHERE  is_active = TRUE";	
 		final List<User> res = ConnHandler.getManager().createNativeQuery(sql, User.class).getResultList();
 		
@@ -23,30 +23,42 @@ public class UserDao extends BaseMasterDao<User>{
 	}
 
 	@Override
-	Optional<User> getById(Long id) {
+	public Optional<User> getById(Long id) {
 		return Optional.ofNullable(super.getById(User.class, id));
 	}
 
 	
 	
 	@Override
-	Optional<User> getByIdRef(Long id) {
+	public Optional<User> getByIdRef(Long id) {
 		return Optional.ofNullable(super.getByIdRef(User.class, id));
 	}
 	
-	@SuppressWarnings("hiding")
-	@Override
-	public <User extends BaseEntity> User save(User entity) {
-
-		return super.save(entity);
+	
+	@SuppressWarnings("unchecked")
+	public Optional<User> login(String email){
+		List<User> users = new ArrayList<>();
+		try {
+		
+			StringBuilder sqlQuery = new StringBuilder();
+			 sqlQuery.append("SELECT u.id, u.email, r.role_code, u.is_active FROM t_user u ");
+			 sqlQuery.append("INNER JOIN t_role r ON r.id = u.role_id ");
+			 sqlQuery.append("WHERE u.email= :email AND u.is_active = TRUE");
+			 
+			 users = ConnHandler.getManager().createNativeQuery((sqlQuery.toString()), User.class).setParameter("email", email).getResultList();
+		}
+		catch (final Exception e) {
+			e.printStackTrace();
+		}
+		return Optional.ofNullable(users.get(0));
+		
+		
 	}
 	
 	
-	@SuppressWarnings("hiding")
-	@Override
-	public <User extends BaseEntity> User saveAndFlush(User entity) {
-		return super.saveAndFlush(entity);
-	}
+	
+	
+	
 	
 	
 	
