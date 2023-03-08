b8678bd7703423b43d23d7212c55dcb5f9f4b2df
sylsblxx <syalsabilaameliaputri@gmail.com>
2023-03-08 16:44:09 +0700

    Add pojo file

---
+++ community/src/main/java/com/lawencon/community/profile/pojo/PojoProfileVerificationRes.java
@@ -0,0 +1,13 @@
+package com.lawencon.community.profile.pojo;
+
+public class PojoProfileVerificationRes {
+	private String verificationCode;
+
+	public String getVerificationCode() {
+		return verificationCode;
+	}
+
+	public void setVerificationCode(String verificationCode) {
+		this.verificationCode = verificationCode;
+	}
+}
