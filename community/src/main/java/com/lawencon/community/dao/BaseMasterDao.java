f8e4c7d2813512c640b0448cd583db3e45c5a53c
Anisa Zelkia <zelkiaanisa@gmail.com>
2023-03-08 10:57:53 +0700

    create master dao

---
+++ community/src/main/java/com/lawencon/community/dao/BaseMasterDao.java
@@ -0,0 +1,13 @@
+package com.lawencon.community.dao;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.lawencon.base.AbstractJpaDao;
+
+
+public abstract class BaseMasterDao<T> extends AbstractJpaDao {
+    abstract List<T> getAll();
+
+    abstract Optional<T> getById(Long id);
+}
