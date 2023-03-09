81ee6ad73c1011d5541f8f630c8e981dd23b96e9
Ari Prayoga Hutabarat <ariprayoga007@gmail.com>
2023-03-09 10:47:36 +0700

    Add user service

--- community/src/main/java/com/lawencon/community/service/UserService.java
+++ community/src/main/java/com/lawencon/community/service/UserService.java
@@ -3,14 +3,28 @@ package com.lawencon.community.service;
 import java.util.ArrayList;
 import java.util.Optional;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 
 import com.lawencon.community.dao.UserDao;
 import com.lawencon.community.model.User;
 
-public class UserService {
+@Service
+public class UserService implements UserDetailsService{
 	private UserDao userDao;
+	
+	@Autowired
+	private PasswordEncoder encoder;
+	
+	@PersistenceContext
+	private EntityManager em;
 	
 	public UserService(UserDao userDao) {
 		this.userDao = userDao;
@@ -26,7 +40,10 @@ public class UserService {
 		}
 
 		throw new UsernameNotFoundException("Wrong email and password!");
-
+	}
+	
+	public Optional<User> login(final String email) {
+		return userDao.login(email);
 	}
 
 }
