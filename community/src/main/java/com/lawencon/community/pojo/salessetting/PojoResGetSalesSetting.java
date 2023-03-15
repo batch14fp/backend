ef2b4817a12056d5df6160b73925c9c73a1f8413
Ari Prayoga Hutabarat <ariprayoga007@gmail.com>
2023-03-15 15:33:11 +0700

    add sales service

---
+++ community/src/main/java/com/lawencon/community/pojo/salessetting/PojoResGetSalesSetting.java
@@ -0,0 +1,30 @@
+package com.lawencon.community.pojo.salessetting;
+
+public class PojoResGetSalesSetting {
+	private float system_income;
+	private float member_income;
+	private float tax;
+	
+	public float getSystem_income() {
+		return system_income;
+	}
+	public void setSystem_income(float system_income) {
+		this.system_income = system_income;
+	}
+	public float getMember_income() {
+		return member_income;
+	}
+	public void setMember_income(float member_income) {
+		this.member_income = member_income;
+	}
+	public float getTax() {
+		return tax;
+	}
+	public void setTax(float tax) {
+		this.tax = tax;
+	}
+	
+	
+	
+
+}
