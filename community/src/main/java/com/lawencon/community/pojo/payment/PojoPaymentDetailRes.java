ecb80598d4f1e8a5f37b662c1ddc17ccd8316316
Anisa Zelkia <zelkiaanisa@gmail.com>
2023-03-30 16:41:30 +0700

    update payment by invoice id

--- community/src/main/java/com/lawencon/community/pojo/payment/PojoPaymentDetailRes.java
+++ community/src/main/java/com/lawencon/community/pojo/payment/PojoPaymentDetailRes.java
@@ -20,6 +20,35 @@ public class PojoPaymentDetailRes {
 	private String imageActivity;
 	private LocalDateTime startDate;
 	private LocalDateTime endDate;
+	
+	private String codeStatus;
+	private String statusName;
+	private Integer periodDay;
+	private BigDecimal price;
+	public String getCodeStatus() {
+		return codeStatus;
+	}
+	public void setCodeStatus(String codeStatus) {
+		this.codeStatus = codeStatus;
+	}
+	public String getStatusName() {
+		return statusName;
+	}
+	public void setStatusName(String statusName) {
+		this.statusName = statusName;
+	}
+	public Integer getPeriodDay() {
+		return periodDay;
+	}
+	public void setPeriodDay(Integer periodDay) {
+		this.periodDay = periodDay;
+	}
+	public BigDecimal getPrice() {
+		return price;
+	}
+	public void setPrice(BigDecimal price) {
+		this.price = price;
+	}
 	public String getActivityId() {
 		return activityId;
 	}
