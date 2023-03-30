0347476da362e6dcfe4f6202afb14afdb18b3e73
Anisa Zelkia <zelkiaanisa@gmail.com>
2023-03-30 14:11:46 +0700

    update service

---
+++ community/src/main/java/com/lawencon/community/pojo/payment/PojoAdminPaymentReqUpdate.java
@@ -0,0 +1,24 @@
+package com.lawencon.community.pojo.payment;
+
+public class PojoAdmimPaymentReqUpdate {
+	private String paymentId;
+	private Boolean isApprove;
+
+	public Boolean getIsApprove() {
+		return isApprove;
+	}
+
+	public void setIsApprove(Boolean isApprove) {
+		this.isApprove = isApprove;
+	}
+
+	public String getPaymentId() {
+		return paymentId;
+	}
+
+	public void setPaymentId(String paymentId) {
+		this.paymentId = paymentId;
+	}
+	
+	
+}
